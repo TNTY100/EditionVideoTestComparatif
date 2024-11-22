0b4b7051c3709a634d8734932da0725a37249058
Yohan Thibault <y2thibaultgm@gmail.com>
2024-11-22 11:50:09 -0500

    Frame translator

--- src/main/java/cal/a24/ffmpeg/FFmpegConvert60fpsTo30.java
+++ src/main/java/cal/a24/ffmpeg/FFmpegConvert60fpsTo30.java
@@ -2,22 +2,23 @@ package cal.a24.ffmpeg;
 
 import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFmpegExecutor;
-import net.bramp.ffmpeg.FFprobe;
 import net.bramp.ffmpeg.builder.FFmpegBuilder;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-public class FFmpegConvert30FpsTo60 {
+public class FFmpegConvert60fpsTo30 {
     public static void main(String[] args) throws IOException {
-
-        String inputFile = "./src/main/resources/videos/48SecondesAvecSon.mp4";
-        String outputFile = "./output/ffmpeg_30to60.mp4";
+        String inputFile = "./src/main/resources/videos/bbb_sunflower_2160p_60fps_normal.mp4";
+        String outputFile = "./output/ffmpeg_60to30.mp4";
 
 
         FFmpeg ffmpeg = new FFmpeg();
         FFmpegBuilder builder = ffmpeg.builder()
                 .addInput(inputFile)
-                .setVideoFilter("fps=fps=60")
+                .setStartOffset(10, TimeUnit.SECONDS)
+                .addExtraArgs("-t", "10")
+                .setVideoFilter("fps=fps=30")
                 .addOutput(outputFile)
                 .done();
 
