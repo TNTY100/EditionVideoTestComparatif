b1cf57a9fbf18dff67a2cc416e996ff068f05711
Yohan Thibault <y2thibaultgm@gmail.com>
2024-11-21 22:11:18 -0500

    Ajout tests 30 fps à 60 fps

---
+++ src/main/java/cal/a24/ffmpeg/FFmpegConvert30FpsTo60.java
@@ -0,0 +1,28 @@
+package cal.a24.ffmpeg;
+
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+
+import java.io.IOException;
+
+public class FFmpegConvert30FpsTo60 {
+    public static void main(String[] args) throws IOException {
+
+        String inputFile = "./src/main/resources/videos/48SecondesAvecSon.mp4";
+        String outputFile = "./output/ffmpeg_30to60.mp4";
+
+
+        FFmpeg ffmpeg = new FFmpeg();
+        FFmpegBuilder builder = ffmpeg.builder()
+                .addInput(inputFile)
+                .setVideoFilter("fps=fps=60")
+                .addOutput(outputFile)
+                .done();
+
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg);
+
+        executor.createJob(builder);
+    }
+}
---
+++ src/main/java/cal/a24/javacv/JavaCVConvert30FpsTo60.java
@@ -0,0 +1,92 @@
+package cal.a24.javacv;
+
+import org.bytedeco.ffmpeg.global.avcodec;
+import org.bytedeco.ffmpeg.global.avutil;
+import org.bytedeco.javacv.*;
+
+public class JavaCVConvert30FpsTo60 {
+    public static void main(String[] args) {
+
+        String inputFile = "./src/main/resources/videos/48SecondesAvecSon.mp4";
+        String outputFile = "./output/javacv_30to60.mp4";
+
+        try {
+            FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputFile);
+            grabber.start();
+
+            FFmpegFrameFilter filter = new FFmpegFrameFilter("fps=fps=60",
+                    "anull",
+                    grabber.getImageWidth(),
+                    grabber.getImageHeight(), grabber.getAudioChannels());
+            filter.setSampleFormat(grabber.getSampleFormat());
+            filter.setSampleRate(grabber.getSampleRate());
+            filter.setPixelFormat(grabber.getPixelFormat());
+            filter.setFrameRate(grabber.getFrameRate());
+            filter.setSampleRate(grabber.getSampleRate());
+            filter.setSampleFormat(grabber.getSampleFormat());
+            filter.start();
+
+            FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(outputFile,
+                    grabber.getImageWidth(),
+                    grabber.getImageHeight(),
+                    grabber.getAudioChannels());
+
+            recorder.setSampleRate(filter.getSampleRate());
+            recorder.setFrameRate(60);
+            recorder.setPixelFormat(avutil.AV_PIX_FMT_YUV420P);
+            recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
+            recorder.setAudioCodec(avcodec.AV_CODEC_ID_AAC);
+            recorder.setFormat("mp4");
+            recorder.setVideoBitrate(grabber.getVideoBitrate());
+            recorder.setAudioBitrate(grabber.getAudioBitrate()/2);
+            recorder.start();
+
+            Frame capturedFrame;
+            Frame pullFrame;
+            while (true) {
+                try {
+                    capturedFrame = grabber.grab();
+                    if (capturedFrame == null) {
+                        System.out.println("ERREUR");
+                        break;
+                    }
+
+                    if (capturedFrame.image != null || capturedFrame.samples != null) {
+                        filter.push(capturedFrame);
+                    }
+                    if ( (pullFrame = filter.pull()) != null) {
+                        if(pullFrame.image != null || pullFrame.samples != null){
+                            recorder.record(pullFrame);
+                        }
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            grabber.stop();
+            grabber.close();
+            filter.stop();
+            filter.close();
+            recorder.stop();
+            recorder.release();
+            recorder.close();
+
+        } catch (FFmpegFrameGrabber.Exception e) {
+            throw new RuntimeException(e);
+        } catch (FrameGrabber.Exception e) {
+            throw new RuntimeException(e);
+        } catch (FFmpegFrameRecorder.Exception e) {
+            throw new RuntimeException(e);
+        } catch (FrameRecorder.Exception e) {
+            throw new RuntimeException(e);
+        } catch (FFmpegFrameFilter.Exception e) {
+            throw new RuntimeException(e);
+        } catch (FrameFilter.Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
